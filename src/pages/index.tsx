c8dd2236defa1143308be26f84031e36978e8306
Erick Clinton <erickleite2009321@gmail.com>
2023-03-31 09:58:36 -0300

    Adding component header

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { styled } from "../styles"
+
+
+const Button = styled('button',{
+  backgroundColor:'$green500',
+  borderRadius: 8,
+  border:0,
+  padding:'5px 8px',
+
+  span:{
+    fontWeight:'bold'
+  },
+
+  '&:hover':{
+    filter:'brightness(0.8)'
+  }
+})
+
+export default function Home() {
+  return (
+   <Button>  
+    <span>teste</span>
+    Hello Word
+    
+    
+    </Button>
+  )
+}
